33169bca125637e6cf716202aa0a1d18f81bd9fa
DS484 <nguyenducsang9a@gmail.com>
2024-04-28 15:00:10 +0700

    New verson

--- FSignIn/FStatistical.Designer.cs
+++ FSignIn/FStatistical.Designer.cs
@@ -43,16 +43,26 @@
             Guna.Charts.WinForms.ChartFont chartFont7 = new Guna.Charts.WinForms.ChartFont();
             Guna.Charts.WinForms.Tick tick3 = new Guna.Charts.WinForms.Tick();
             Guna.Charts.WinForms.ChartFont chartFont8 = new Guna.Charts.WinForms.ChartFont();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FStatistical));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             chartInfo = new Guna.Charts.WinForms.GunaChart();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            label1 = new Label();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // chartInfo
             // 
             chartFont1.FontName = "Arial";
             chartInfo.Legend.LabelFont = chartFont1;
-            chartInfo.Location = new Point(108, 67);
+            chartInfo.Location = new Point(120, 70);
             chartInfo.Name = "chartInfo";
-            chartInfo.Size = new Size(848, 566);
+            chartInfo.Size = new Size(887, 577);
             chartInfo.TabIndex = 0;
             chartFont2.FontName = "Arial";
             chartFont2.Size = 12;
@@ -80,20 +90,80 @@
             tick3.Font = chartFont8;
             chartInfo.ZAxes.Ticks = tick3;
             // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            guna2HtmlLabel1.Location = new Point(318, 25);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(400, 27);
+            guna2HtmlLabel1.TabIndex = 1;
+            guna2HtmlLabel1.Text = "Biểu đồ thống kê doanh thu của các khách sạn";
+            guna2HtmlLabel1.TextAlignment = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(731, 674);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 2;
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.Image = (Image)resources.GetObject("guna2PictureBox1.Image");
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(941, 12);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(66, 52);
+            guna2PictureBox1.TabIndex = 3;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(3, 70);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(111, 214);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(0, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(15, 14);
+            panel2.TabIndex = 0;
+            // 
             // FStatistical
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1035, 697);
+            ClientSize = new Size(1035, 710);
+            Controls.Add(panel1);
+            Controls.Add(guna2PictureBox1);
+            Controls.Add(label1);
+            Controls.Add(guna2HtmlLabel1);
             Controls.Add(chartInfo);
             Margin = new Padding(2);
             Name = "FStatistical";
             Text = "FStatistical";
+            Load += FStatistical_Load;
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Guna.Charts.WinForms.GunaChart chartInfo;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
--- FSignIn/FStatistical.cs
+++ FSignIn/FStatistical.cs
@@ -1,4 +1,5 @@
 ﻿using Guna.Charts.WinForms;
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,22 +9,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataTable = System.Data.DataTable;
+using Point = System.Drawing.Point;
 
 namespace Hotel_Management
 {
     public partial class FStatistical : Form
     {
+        private string selected = "";
         private DataGridView dgv;
 
         public FStatistical(DataGridView dgv)
         {
             InitializeComponent();
             this.dgv = dgv;
+            //DoughnutChart();
         }
 
+        public void DoughnutChart()
+        {
+            //string[] months = { "January", "February", "March", "April" };
+
+            //chartInfo.Title.Text = "Doughnut Chart";
+            //chartInfo.Legend.Position = Guna.Charts.WinForms.LegendPosition.Right;
+            //chartInfo.XAxes.Display = false;
+            //chartInfo.YAxes.Display = false;
+
+            //var dataset = new Guna.Charts.WinForms.GunaDoughnutDataset();
+            //var r = new Random();
+            //for (int i = 0; i < months.Length; i++)
+            //{
+            //    int num = r.Next(10, 100);
+
+            //    dataset.DataPoints.Add(months[i], num);
+            //}
+
+            //chartInfo.Datasets.Add(dataset);
+
+            //chartInfo.Update();
+
+            
+        }
+
+        private void LoadChartData()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("HotelName", typeof(string));
+            dataTable.Columns.Add("Value", typeof(int));
+
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    dataTable.Rows.Add(row.Cells["Tên khách sạn"].Value.ToString(),
+                                       Convert.ToInt32(row.Cells["Tổng doanh thu"].Value));
+                }
+            }
+
+            var dataset = new GunaDoughnutDataset();
+
+            int total = 0;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                total += Convert.ToInt32(row["Value"]);
+            }
+
+            // Add data to the dataset with percentage and sign
+            foreach (DataRow row in dataTable.Rows)
+            {
+                int value = Convert.ToInt32(row["Value"]);
+                double percentage = ((double)value / total) * 100;
+                string label = $"{row["HotelName"].ToString()} ({percentage.ToString("0.00")}%)";
+                dataset.DataPoints.Add(label, value);
+            }
+
+            chartInfo.Datasets.Add(dataset);
 
 
 
+            chartInfo.Update();
+        }
 
+        public void HorizontalBarChart()
+        {
+            List<string> hotelName = new List<string>();
+            List<int> totalRevenue = new List<int>();
+
+            int total = totalRevenue.Sum();
+
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    hotelName.Add(row.Cells[1].Value.ToString()!);
+                    totalRevenue.Add(Convert.ToInt32(row.Cells[4].Value!));
+                }
+            }
+
+            var dataset = new Guna.Charts.WinForms.GunaHorizontalBarDataset();
+            for (int i = 0; i < hotelName.Count; i++)
+            {
+                dataset.DataPoints.Add(hotelName[i], totalRevenue[i]);
+            }
+
+            chartInfo.Datasets.Add(dataset);
+
+            chartInfo.Update();
+
+        }
+
+        private void FStatistical_Load(object sender, EventArgs e)
+        {
+            string[] typeChart = { "Cột", "Tròn" };
+            panel1.BackColor = Color.FromArgb(20, 0, 0, 0);
+
+
+            for (int i = typeChart.Length - 1; i >= 0; i--)
+            {
+                var button = new System.Windows.Forms.Button()
+                {
+                    BackColor = Color.Empty,
+                    Text = typeChart[i],
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    Dock = DockStyle.Top,
+                    Height = 25,
+                    FlatStyle = FlatStyle.Flat,
+                    Padding = new Padding(10, 0, 0, 0),
+                    FlatAppearance =
+                    {
+                         BorderSize = 0,
+                         MouseOverBackColor = Color.DodgerBlue
+                    }
+                };
+                button.Click += (s, evnt) =>
+                {
+                    selected = button.Text;
+                    SelectBasicExamples(selected);
+                    panel2.Size = new Size(5, button.Height);
+                    panel2.Location = new Point(panel1.Width - 5, button.Top);
+                    panel2.BringToFront();
+                };
+                panel1.Controls.Add(button);
+            }
+        }
+
+        private void SelectBasicExamples(string name)
+        {
+            chartInfo.Datasets.Clear();
+
+            if (name == "Cột")
+            {
+                HorizontalBarChart();
+            }
+            else if (name == "Tròn")
+            {
+                LoadChartData();
+            }
+        }
     }
 }
