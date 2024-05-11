7021f4804f3cdda84287d0ef94cb7b59fdda2b65
tuantran1001 <tuantran.it.net@gmail.com>
2024-05-11 10:12:38 +0700

    edit FReason

--- FSignIn/FReason.Designer.cs
+++ FSignIn/FReason.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2ShadowPanel3 = new Guna.UI2.WinForms.Guna2ShadowPanel();
@@ -42,6 +43,7 @@
             lblHi = new Guna.UI2.WinForms.Guna2HtmlLabel();
             guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             label3 = new Label();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
             guna2ShadowPanel3.SuspendLayout();
             SuspendLayout();
             // 
@@ -59,13 +61,13 @@
             guna2ShadowPanel3.Controls.Add(lblHi);
             guna2ShadowPanel3.Controls.Add(guna2HtmlLabel1);
             guna2ShadowPanel3.Controls.Add(label3);
-            guna2ShadowPanel3.FillColor = Color.White;
-            guna2ShadowPanel3.Location = new Point(25, 1);
+            guna2ShadowPanel3.FillColor = Color.WhiteSmoke;
+            guna2ShadowPanel3.Location = new Point(12, 12);
             guna2ShadowPanel3.Name = "guna2ShadowPanel3";
             guna2ShadowPanel3.Radius = 10;
             guna2ShadowPanel3.ShadowColor = Color.Silver;
-            guna2ShadowPanel3.ShadowShift = 18;
-            guna2ShadowPanel3.Size = new Size(483, 569);
+            guna2ShadowPanel3.ShadowShift = 0;
+            guna2ShadowPanel3.Size = new Size(486, 577);
             guna2ShadowPanel3.TabIndex = 86;
             // 
             // txtOther
@@ -113,32 +115,32 @@
             // btnCancelRoom
             // 
             btnCancelRoom.BorderRadius = 10;
-            btnCancelRoom.BorderThickness = 2;
+            btnCancelRoom.BorderThickness = 1;
             btnCancelRoom.CustomizableEdges = customizableEdges1;
-            btnCancelRoom.DefaultAutoSize = true;
             btnCancelRoom.DisabledState.BorderColor = Color.DarkGray;
             btnCancelRoom.DisabledState.CustomBorderColor = Color.DarkGray;
             btnCancelRoom.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             btnCancelRoom.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnCancelRoom.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCancelRoom.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             btnCancelRoom.ForeColor = Color.White;
-            btnCancelRoom.Location = new Point(198, 499);
+            btnCancelRoom.Location = new Point(188, 499);
             btnCancelRoom.Margin = new Padding(2, 3, 2, 3);
             btnCancelRoom.Name = "btnCancelRoom";
             btnCancelRoom.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            btnCancelRoom.Size = new Size(112, 34);
+            btnCancelRoom.Size = new Size(131, 42);
             btnCancelRoom.TabIndex = 97;
             btnCancelRoom.Text = "Trả phòng";
             btnCancelRoom.Click += btnCancelRoom_Click;
             // 
             // guna2HtmlLabel3
             // 
+            guna2HtmlLabel3.AutoSize = false;
             guna2HtmlLabel3.BackColor = Color.Transparent;
             guna2HtmlLabel3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
             guna2HtmlLabel3.Location = new Point(56, 361);
             guna2HtmlLabel3.Margin = new Padding(2, 3, 2, 3);
             guna2HtmlLabel3.Name = "guna2HtmlLabel3";
-            guna2HtmlLabel3.Size = new Size(44, 25);
+            guna2HtmlLabel3.Size = new Size(53, 37);
             guna2HtmlLabel3.TabIndex = 95;
             guna2HtmlLabel3.Text = "Khác:";
             // 
@@ -155,12 +157,13 @@
             // 
             // guna2HtmlLabel2
             // 
+            guna2HtmlLabel2.AutoSize = false;
             guna2HtmlLabel2.BackColor = Color.Transparent;
             guna2HtmlLabel2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
             guna2HtmlLabel2.Location = new Point(55, 165);
             guna2HtmlLabel2.Margin = new Padding(2, 3, 2, 3);
             guna2HtmlLabel2.Name = "guna2HtmlLabel2";
-            guna2HtmlLabel2.Size = new Size(205, 25);
+            guna2HtmlLabel2.Size = new Size(205, 36);
             guna2HtmlLabel2.TabIndex = 89;
             guna2HtmlLabel2.Text = "Lý do bạn trả phòng là gì?";
             // 
@@ -173,7 +176,7 @@
             lblHi.Location = new Point(55, 121);
             lblHi.Margin = new Padding(1);
             lblHi.Name = "lblHi";
-            lblHi.Size = new Size(354, 31);
+            lblHi.Size = new Size(354, 40);
             lblHi.TabIndex = 88;
             lblHi.Text = "Xin chào, ";
             lblHi.TextAlignment = ContentAlignment.MiddleLeft;
@@ -195,19 +198,27 @@
             label3.ForeColor = Color.Black;
             label3.Location = new Point(0, 0);
             label3.Name = "label3";
-            label3.Size = new Size(483, 95);
+            label3.Size = new Size(486, 95);
             label3.TabIndex = 69;
             label3.Text = "Lý do trả phòng";
             label3.TextAlign = ContentAlignment.BottomCenter;
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 18;
+            guna2Elipse1.TargetControl = this;
             // 
             // FReason
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(535, 564);
+            BackColor = Color.DimGray;
+            ClientSize = new Size(510, 601);
             Controls.Add(guna2ShadowPanel3);
+            FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(2, 3, 2, 3);
             Name = "FReason";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "FReason";
             guna2ShadowPanel3.ResumeLayout(false);
             guna2ShadowPanel3.PerformLayout();
@@ -233,5 +244,6 @@
         private CheckBox cbReason4;
         private CheckBox cbReason3;
         private CheckBox cbReason2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
     }
 }
